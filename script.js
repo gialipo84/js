c7900d13a98466a2c32a5dcca05e0e3c74a2eb79
gialipo84 <97461693+gialipo84@users.noreply.github.com>
2022-03-11 23:01:43 +0400

    js

---
+++ script.js
@@ -0,0 +1,72 @@
+// სავარჯიშო N1
+// let user = {
+//     firstname:'giorgi',
+//     lastname: 'smith',
+//     age: 25,
+//     studentstatus:'active'
+
+// };
+// console.log(user.stu
+    
+} else {
+    
+
+
+// სავარჯიშო N2
+// პირველი გზა
+// let testmasive = [1,5,3,8,7,6]
+
+// for (i=0;i<testmasive.length;i++)
+
+// console.log(testmasive[i]);
+
+// მეორე გზა
+
+// let testmasive2=[2,5,6,9,4,3,7,22]
+
+// for (let item of testmasive2){
+//     console.log(item);
+// }
+
+// სავარჯიშო N3
+
+// let numbers = [5,6,10,25,3,7,4,100,36];
+// for (i=0;i < numbers.length; i++){
+//     if (numbers[i]>5) {
+//         console.log(numbers[i]);
+//     }
+// }
+
+
+// სავარჯიშო N4
+// let user ={
+//     name: 'giorgi',
+//     age: 20,
+//     studentstatus: 'active'}
+
+
+// let username='giorgi',
+//     age=20
+//     studentstatus='active'
+
+// if ( age>18, studentstatus='active' ){
+//     console.log('hello');
+// } 
+// if(username='giorgi'){
+//     console.log('hello giorgi');
+// }
+// if (studentstatus='active' || age<15){
+//     console.log('hello wordl');
+// }
+// else {
+//     console.log('error');
+
+// }
+
+
+
+
+
+
+
+
